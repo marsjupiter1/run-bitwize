75c51ca9ae8331dc8b3b0cd9209b1ca3da38901c
Martin Walker <martin@marsjupiter.com>
2021-09-13 10:48:27 +0100

    test commit

---
+++ src/bitwize.rs
@@ -0,0 +1,24 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+pub struct BVec{
+    bits_per_value: u8,
+    capacity: usize,
+    values: Vec<u8>,
+}
+
+fn new() -> BVec{
+
+    with_capacity(16)
+}
+
+fn with_capacity(capacity:usize)-> BVec{
+
+    let v = Vec::with_capacity(capacity);
+    BVec{
+        bits_per_value: 2,
+        capacity: capacity,
+        values: v,
+    }
+
+}
